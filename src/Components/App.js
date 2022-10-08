3cac0727143544809dd270de7cc3aeeb1e2fb2cf
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2022-10-08 08:54:00 +0300

    edited the file

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,23 @@
 import React from 'react'
-import Home from './Home'
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Store from './pages/Store';
+import About from './pages/About';
+import NavBar from './NavBar';
+
+
 
 function App() {
   return (
-    <div>
-    <Home />
-    </div>
-  )
+    <div className='mb-4'>
+      <>
+    <NavBar />
+   <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/store" element={<Store />} />
+    </Routes>
+      </>
+    </div>)
 }
-
-export default App
+export default App;
