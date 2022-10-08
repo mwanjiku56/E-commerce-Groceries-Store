7c023a72bba4d0eb5665731b7e543217eeb36fb8
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2022-10-08 08:51:40 +0300

    imported productlist

---
+++ src/Components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ProductsList from '../ProductsList'
+
+function Home() {
+  return (
+    <div>
+    <h1>
+      <ProductsList />
+    </h1>
+    </div>
+  )
+}
+
+export default Home
